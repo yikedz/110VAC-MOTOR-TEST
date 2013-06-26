6d1baf0fecef7e41b7b951478a86ca1fc94126c0
yikedz <huguangwei999@163.com>
2013-06-26 14:38:48 +0800

    蒋师傅工装程序  电机正反转 90度，两个传感器 1个按钮开关

--- keil/main.c
+++ keil/main.c
@@ -1,90 +1,44 @@
 /*
-	单向异步电机pwm调速 
 	mcu：stc89c52
-	电脑控制
 */
 #include<reg51.h>
-#include "modbus_slave.h"
-#include"uart.h"
-#include "soft_timer.h"
 
 #define ON 0
 #define OFF 1
-
+sbit key = P1^5;
+sbit sensor1 = P1^6;
+sbit sensor2 = P1^7;
 
 sbit FZ = P2^7;
 sbit ZZ = P2^4;	
+			 
 
-static unsigned char started_flg = 0;
-
-static unsigned char step = 1;					 
-							
-unsigned char time_getter[4];//电机运行时间数组 0--正转时间 1 停止时间1  2反转时间 3停止时间2
+unsigned char key_pressed = 0;//保存按键按下
+unsigned char sensor1_flg = 0;//保存sensor1信号状态 
+unsigned char sensor2_flg = 0;//保存sensor1信号状态
+unsigned char  flg_10ms = 0;
 
 void init_timer0(void);
-
-void run_motor(void);
+void get_io_status(void);//获取io状态
+void system_handle(void);
 
 void main(void)
 {
 	FZ = OFF;
  	ZZ = OFF;
-
-
-	//电机运行时间初始值
-	time_getter[0] = 45;//正转
-
-	time_getter[1] = 1; //停止时间1
-
-	time_getter[2] = 4; //反转
-
-	time_getter[3] = 65;//停止时间2
-
-	init_uart();
-
 	init_timer0();
-
-
-	 modbus_reg[0] = 45;
-	 modbus_reg[1] = 1;
-	 modbus_reg[2]  = 4;
-	 modbus_reg[3] = 65;
-
-	 modbus_reg[4]  = 1;//启动
-
-	 modbus_reg[5]  = 1;//动作状态步
-
-	 modbus_reg[6] = 1;//电机运行标志
-
-	 modbus_reg[7] = 0;//循环次数
-
-
 	EA = 1;
-	
 	while(1)
 	{
-		if( modbus_reg[4]==1)
+		if(flg_10ms)
 		{
-			run_motor();
-			modbus_reg[5] = step;	
+			flg_10ms = 0;
+			get_io_status();
 		}
-		else
-		{
-			//停止的时候可以设置时间
-			time_getter[0] = modbus_reg[0];
-			time_getter[1] = modbus_reg[1];
-			time_getter[2] = modbus_reg[2];
-			time_getter[3] = modbus_reg[3];
-			modbus_reg[5] = 0;//动作状态步
-			FZ  = 1;
-			ZZ = 1;
-			started_flg = 0;
-			step = 1;
-			//modbus_reg[7] = 0;
-		}
-
-		modbus_decode(); 
+		
+		system_handle();
 	}
+	
 }	
 void init_timer0(void)
 {
@@ -103,93 +57,217 @@ void timer0_isr(void) interrupt 1
 	TH0 = 0xdc;//11.0592
 	TL0 = 0x00;
 
-	update_all_timers();
+    flg_10ms = 1;
+	//update_all_timers();
 
 }
-
-void run_motor(void)
+void get_io_status(void)//获取io状态
 {
+static unsigned char key_state = 1;
+static unsigned char key_pressed_count = 0;
+static unsigned char key_release_count = 0;
 
+static unsigned char sensor1_state = 1;
+static unsigned char sensor1_pressed_count = 0;
+static unsigned char sensor1_release_count = 0;
 
-	switch(step)
+static unsigned char sensor2_state = 1;
+static unsigned char sensor2_pressed_count = 0;
+static unsigned char sensor2_release_count = 0;
+ 
+//key button =================================
+	switch(key_state)
 	{
-		case 1:
-		modbus_reg[6] = 1;	
-		
-		if(started_flg==0)
-		{
-			start_soft_timer(SOFT_TIMER0,time_getter[0]*100);
-			ZZ = 0;//电机正转
-			started_flg = 1;			 
-		}
-		else
-		{
-			if(check_soft_timer(SOFT_TIMER0))
+		case 1://检查低电平
+			key = 1;
+			if(key==0)//按键按下
 			{
-				step = 2;
-				started_flg = 0;		
+				key_pressed_count++;
+				if(key_pressed_count==10)
+				{
+					key_pressed = 1;
+					key_pressed_count = 0;
+				}	
+			}
+			else
+			{
+				key_state = 2;
+				key_pressed_count = 0;
+				key_release_count = 0;
+	
 			}	
-		}
 		break;
 		case 2:
-		modbus_reg[6] = 0;	
-		if(started_flg==0)
-		{
-			start_soft_timer(SOFT_TIMER0,time_getter[1]*100);
-			ZZ = 1;//电机停止时间1
-			started_flg = 1;	
-		}
-		else
+			key = 1;
+			if(key)//按键按下
+			{
+				key_release_count++;
+				if(key_release_count==10)
+				{
+					key_pressed = 0;
+					key_release_count = 0;
+				}	
+			}
+			else
+			{
+				key_state = 1;
+				key_pressed_count = 0;
+				key_release_count = 0;
 	
-		{
-			if(check_soft_timer(SOFT_TIMER0))
-			{
-				step = 3;
-				started_flg = 0;		
-			}	
-		}
-		break;
-		case 3:
-		modbus_reg[6] = 1;	
-		if(started_flg==0)
-		{
-			start_soft_timer(SOFT_TIMER0,time_getter[2]*100);
-			FZ = 0;//电机反转
-			started_flg = 1;	
-		}
-		else
-		{
-			if(check_soft_timer(SOFT_TIMER0))
-			{
-				step = 4;
-				started_flg = 0;		
-			}	
-		}
-		break;
-		case 4:
-		modbus_reg[6] = 0;	
-		if(started_flg==0)
-		{
-			start_soft_timer(SOFT_TIMER0,time_getter[3]*100);
-			FZ = 1;//电机停止时间2
-			started_flg = 1;	
-		}
-		else
-		{
-			if(check_soft_timer(SOFT_TIMER0))
-			{
-				step = 1;
-				started_flg = 0;	
-				modbus_reg[7]++;	
-			}	
-		}
+			}
 		break;
 		default:
-			step = 1;
-			started_flg = 0;
+			key_state = 1;
 		break;	
 	}
+//key button =================================
+
+//sensor1=====================================
+	switch(sensor1_state)
+	{
+		case 1:
+			sensor1 =1;
+			if(sensor1==0)//低电平信号有效
+			{
+				sensor1_pressed_count++;
+				if(sensor1_pressed_count==5)
+				{
+					sensor1_flg = 1;
+					sensor1_pressed_count = 0;	
+				}
+			}
+			else
+			{
+				sensor1_state = 2;
+				sensor1_pressed_count = 0;	
+				sensor1_release_count = 0;	
+			}
+		break;
+		case 2:
+			sensor1 =1;
+			if(sensor1)
+			{
+				sensor1_release_count++;
+				if(sensor1_release_count==5)
+				{
+					sensor1_flg = 0;
+					sensor1_release_count = 0;	
+				}
+			}
+			else
+			{
+				sensor1_state = 1;
+				sensor1_pressed_count = 0;	
+				sensor1_release_count = 0;				
+			}
+		break;
+		default:
+			sensor1_state = 1;
+		break;
+	}
+//sensor1=====================================
+
+//sensor2=====================================
+	switch(sensor2_state)
+	{
+		case 1:
+			sensor2 =1;
+			if(sensor2==0)//低电平信号有效
+			{
+				sensor2_pressed_count++;
+				if(sensor2_pressed_count==5)
+				{
+					sensor2_flg = 1;
+					sensor2_pressed_count = 0;	
+				}
+			}
+			else
+			{
+				sensor2_state = 2;
+				sensor2_pressed_count = 0;	
+				sensor2_release_count = 0;	
+			}
+		break;
+		case 2:
+			sensor2 =1;
+			if(sensor2)
+			{
+				sensor2_release_count++;
+				if(sensor2_release_count==5)
+				{
+					sensor2_flg = 0;
+					sensor2_release_count = 0;	
+				}
+			}
+			else
+			{
+				sensor2_state = 1;
+				sensor2_pressed_count = 0;	
+				sensor2_release_count = 0;				
+			}
+		break;
+		default:
+			sensor2_state = 1;
+		break;
+	}
+//sensor2=====================================	
 }
+
+void system_handle(void)
+{
+	static unsigned char key_system_state = 1;
+	static unsigned char pos = 1;
+	static unsigned char  en_motor_run = 0;
+	switch(key_system_state)
+	{
+		case 1://等待按键按下
+			if(key_pressed)//按键按下了
+			{
+				key_system_state = 2;
+			}		
+		break;
+		case 2:
+			if(key_pressed==0)//等待按键松开
+			{
+				en_motor_run = 1;
+				key_system_state = 1;
+			}
+		break;
+		default:
+			key_system_state = 1;	
+		break;
+		
+	}
+	if(en_motor_run)//允许电机转动
+	{
+		switch(pos)
+		{
+			case 1://此时默认电机停止在传感器sensor1位置处 正转到sensor2处
+				ZZ = ON;//电机开始正转
+				if(sensor1_flg)//正转到sensor2处
+				{
+					ZZ = OFF;//正转停止
+					en_motor_run = 0;
+					pos = 2;
+				}
+			break;
+			case 2://此时默认电机停止在传感器sensor2位置处 反转到sensor1处
+				FZ = ON;//电机开始反转
+				if(sensor2_flg)//反转到sensor1处
+				{
+					FZ = OFF;//反转停止
+					en_motor_run = 0;
+					pos = 1;
+				}			
+			break;
+			default:
+				pos = 1;			
+			break;
+		}		
+	}
+
+}
+
 
 
 
